c8a8aa494cb1fce076a76f518f3e69097e8e818f
Davis Goldstein <davisrgoldstein@gmail.com>
2019-01-08 21:15:35 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-  "afoot, Excellent, incredible, Absolute, unreal, Inconceivable, octopus".scan(/w+aeiou/)
+  .scan(/w+aeiou/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
