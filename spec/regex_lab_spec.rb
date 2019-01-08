e60306dbc90ecd29c48b0870f41efd08d2df67cc
Davis Goldstein <davisrgoldstein@gmail.com>
2019-01-08 21:06:36 +0000

    Automatically backed up by Learn

--- spec/regex_lab_spec.rb
+++ spec/regex_lab_spec.rb
@@ -5,7 +5,7 @@ describe "Working with Regular expressions" do
     it "returns true for words starting with a vowel" do
       match = %w{ afoot Excellent incredible Absolute unreal Inconceivable octopus }
 
-      scanl.each do |word|
+      scan.each do |word|
         expect(starts_with_a_vowel?(word)).to be(true)
       end
     end
